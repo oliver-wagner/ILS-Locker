da8ec736aa1bcb58a90c5be1658cbdf3d8f4b327
Oliver Wagner <wagnerof@mail.uc.edu>
2018-04-02 19:38:16 -0400

    Updated wifi connect

--- ArduinoLocker2/ArduinoLocker2.ino
+++ ArduinoLocker2/ArduinoLocker2.ino
@@ -42,36 +42,24 @@ void connect_wifi() {
   }
   else {
     // The RTC data was not valid, so make a regular connection
-  WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
   }
   
-  int retries = 0;
-  int wifiStatus = WiFi.status();
-  while( wifiStatus != WL_CONNECTED ) {
-    Serial.println("Failed to connect to WiFi");
-    retries++;
-    Serial.println("Failed to connect to WiFi");
-    if( retries == 100 ) {
-      // Quick connect is not working, reset WiFi and try regular connection
-      WiFi.disconnect();
-      delay( 10 );
-      WiFi.forceSleepBegin();
-      delay( 10 );
-      WiFi.forceSleepWake();
-      delay( 10 );
-      WiFi.begin(ssid, password);
-    }
-    if( retries == 600 ) {
-      // Giving up after 30 seconds and going back to sleep
-      WiFi.disconnect( true );
-      delay(1);
-      WiFi.mode( WIFI_OFF );
+  unsigned long wifiConnectStart = millis();
+  while( WiFi.status() != WL_CONNECTED ) {
+    Serial.println("Connecting to Wifi");
+    if (WiFi.status() == WL_CONNECT_FAILED) {
       Serial.println("Failed to connect to WiFi. Please verify credentials: ");
-      ESP.deepSleep( 1000 , WAKE_RF_DISABLED );
-      return; // Not expecting this to be called, the previous call will never return.
+      delay(10000);
+      ESP.deepSleep(0);
     }
-    delay( 500 );
-    wifiStatus = WiFi.status();
+    delay(500);
+    Serial.println("...");
+    // Only try for 5 seconds.
+    if (millis() - wifiConnectStart > 15000) {
+      Serial.println("Failed to connect to WiFi");
+      ESP.deepSleep(0);
+    }
   }
 
   Serial.println("");
